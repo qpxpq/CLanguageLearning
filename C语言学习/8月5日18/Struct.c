f4028366f6f0b001817d8e7da56bd9c55eac6055
qpxpq <2568884002@qq.com>
2021-08-05 19:53:06 +0800

    修改提交

--- C语言学习/8月5日18/Struct.c
+++ C语言学习/8月5日18/Struct.c
@@ -1,36 +1,53 @@
-//1、使用struct定义一个结构体，结构体里包含( 1、学生姓名（字符串表示） 2、学生班级（数字表示） 3、学生分数（数字表示）)，用sizeof打印出来该结构体的内存大小
-//2、用上面的结构体，打印出来5个不同学生的信息（一行一个学生信息）
+#include "stdio.h"
+#include "string.h"
 
-#include <stdio.h>
 
-int main(){
-    struct{
-    char *name;  //姓名
-    int class;  //班级
-    float score;  //成绩
-    } stu1,stu2,stu3,stu4,stu5;
-    //给结构体成员赋值
-    stu1.name = "Tom";
-    stu1.class = 1;
-    stu1.score = 98;
-    stu2.name = "张三";
-    stu2.class = 2;
-    stu2.score = 110;
-    stu3.name = "李四";
-    stu3.class = 2;
-    stu3.score = 105;
-    stu4.name = "王五";
-    stu4.class = 3;
-    stu4.score = 118;
-    stu5.name = "赵六";
-    stu5.class = 4;
-    stu5.score = 120;
-    //读取结构体成员的值
-    printf("%s的班级是%d，成绩是%.1f！\n",stu1.name,stu1.class,stu1.score);
-    printf("%s的班级是%d，成绩是%.1f！\n",stu2.name,stu2.class,stu2.score);
-    printf("%s的班级是%d，成绩是%.1f！\n",stu3.name,stu3.class,stu3.score);
-    printf("%s的班级是%d，成绩是%.1f！\n",stu4.name,stu4.class,stu4.score);
-    printf("%s的班级是%d，成绩是%.1f！\n",stu5.name,stu5.class,stu5.score);
+#define DEF_STRING          "ABCDE"
+#define DEF_ARRAY_SIZE      (10)
+#define DEF_STUDENT_NUM     (5)
 
-    return 0;
+typedef unsigned char   U8;
+typedef unsigned short  U16;
+typedef unsigned int    U32;
+
+typedef struct 
+{
+    U8 aName[10];  
+    U8 nClass;
+    U8 nScore;
+}S_STUDENT_IFO;
+
+
+static S_STUDENT_IFO sStdIfo[DEF_STUDENT_NUM] = 
+{
+    {"AA", 1, 10},
+    {"BB", 1, 10},
+    {"CC", 1, 10},
+    {"DD", 1, 10},
+    {"EE", 1, 10},
+};
+
+int main(void)
+{
+    U8 aString[DEF_ARRAY_SIZE] = {0};
+
+    for(U8 i=0; i<DEF_ARRAY_SIZE; i++)
+    {
+        aString[i] = i;
+    }
+
+    printf("Len = %d, %d : %s\n", strlen(DEF_STRING), sizeof(DEF_STRING), DEF_STRING);
+
+    printf("Len = %d:\r\n", sizeof(aString));
+    for(U8 i=0; i<DEF_ARRAY_SIZE; i++)
+    {
+        printf("aString[%d] = %d\r\n", i, aString[i]);
+    }
+
+    printf("%d\r\n", sizeof(S_STUDENT_IFO));
+    printf("%d\r\n", sizeof(sStdIfo));
+    for (U8 i = 0; i < DEF_STUDENT_NUM; i++)
+    {
+        printf("Name: %s Class: %d  Score: %d\r\n", sStdIfo[i].aName, sStdIfo[i].nClass, sStdIfo[i].nScore);
+    }
 }
